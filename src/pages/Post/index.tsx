7e4743894f5915f8739138dd5b6517ff6fdc5fcf
João Pedro <jotapedro1231@gmail.com>
2023-05-31 15:16:31 -0300

    Added markdown

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -2,6 +2,9 @@ import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { IPost, PostContext } from '../../contexts/PostContext'
 import { PostHeader } from '../../components/PostHeader'
+import { PostContainer, PostContent } from './styles'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import gfm from 'remark-gfm'
 
 export function Post() {
   const { posts } = useContext(PostContext)
@@ -14,5 +17,12 @@ export function Post() {
     if (selectedPost) setPost(selectedPost)
   }, [id, posts])
 
-  return <PostHeader post={post} />
+  return (
+    <PostContainer>
+      <PostHeader post={post} />
+      <PostContent>
+        <ReactMarkdown remarkPlugins={[gfm]}>{post.body}</ReactMarkdown>
+      </PostContent>
+    </PostContainer>
+  )
 }
---
+++ src/pages/Post/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from 'styled-components'
+
+export const PostContainer = styled.div``
+
+export const PostContent = styled.div`
+  padding: 40px 32px;
+`
